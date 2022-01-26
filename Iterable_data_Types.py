7d440ba8805b5e03d45192b186e962fab9880d2a
shane <qaz56115624@gmail.com>
2022-01-26 15:53:08 +0800

    ok

---
+++ Iterable_data_Types.py
@@ -0,0 +1,33 @@
+# Iterable 資料型態
+# 字串 列表 集合 字典
+
+
+# for 迴圈
+# for 變數名稱 in 可疊代的資料:
+# for x in [3,5,2]:
+#     print(x)
+# for  x in {"a","test",3,10}: # 集合
+#     print(x)
+# dic = {"a":3,"x":5}
+# for x in dic:
+#     print(x)
+#     print(dic[x]) # 取出字典裡的值
+
+
+# 內建函式
+# max(可疊帶資料)
+# result = max([10,2,30,1])
+# print(result)
+# result = max("xaz")
+# print(result)
+# result = max({10,200,105,15,20})
+# print(result)
+# result = max({"x":4,"a":8})
+# print(result)
+
+
+# sorted(可疊帶資料)
+result = sorted("cba")
+print(result)
+result = sorted({10,3,100,-6})
+print(result)
